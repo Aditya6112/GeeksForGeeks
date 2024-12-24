6d5adb39b3cae85f5c54ab3aeba8808b33df3bdf
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2024-12-24 15:45:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Binary Search/binary-search.cpp
+++ Difficulty: Easy/Binary Search/binary-search.cpp
@@ -7,34 +7,23 @@ using namespace std;
 
 // } Driver Code Ends
 // User function template for C++
-
 class Solution {
   public:
     int binarysearch(vector<int> &arr, int k) {
-        int n = arr.size();
-        int start=0, end=n-1;
-        int mid = (start+end)/2;
-        int index = n+1;
-        int cnt = 0;
-        while(cnt<=n/2){
-            
-            if(mid<0 || mid>=n) break;
-            
-            if(arr[mid]>k){
-                end = mid-1;
-            }else if(arr[mid]<k){
-                start = mid+1;
-            }else{
-                index = min(index, mid);
-                end = mid-1;
+        // code here
+        int s=0;
+        int e=arr.size()-1;
+        int ans=-1;
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(arr[m]==k){
+                ans=m;
+                e=m-1;
             }
-            
-            mid = (start+end)/2;
-            
-            cnt++;
+            else if(arr[m]>k)e=m-1;
+            else s=m+1;
         }
-        
-        return index==n+1 ? -1 : index;
+        return ans;
     }
 };
 
