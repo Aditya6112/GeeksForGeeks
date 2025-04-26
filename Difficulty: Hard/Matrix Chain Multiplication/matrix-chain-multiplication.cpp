9c2f9c937d34085026bc0935f94c020e21118892
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2025-04-27 02:33:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -5,24 +5,27 @@ using namespace std;
 
 // } Driver Code Ends
 
-// User function Template for C++
-int static dp[101][101];
 class Solution {
   public:
-    int solve(vector<int>& arr,int i,int j){
-        int mini=INT_MAX;
-        if(i>=j)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        for(int k=i;k<=j-1;k++){
-            int tempAns=solve(arr,i,k)+solve(arr,k+1,j)+(arr[i-1]*arr[k]*arr[j]);
-            mini=min(tempAns,mini);
+  
+    int solve(vector<int> arr,int i,int j,vector<vector<int>>&t)
+    {
+        if(i>=j) return 0;
+        if(t[i][j]!=-1) return t[i][j];
+        int cost = INT_MAX;
+        for(int k=i;k<j;k++)
+        {
+            int ans = solve(arr,i,k,t) + solve(arr,k+1,j,t) + arr[i-1]*arr[k] * arr[j];
+            
+            cost = min(cost,ans);
         }
-        return dp[i][j]=mini;
-    }
+        return t[i][j] = cost;
+    } 
+     
     int matrixMultiplication(vector<int> &arr) {
-        // code here
-        memset(dp,-1,sizeof(dp));
-        return solve(arr,1,arr.size()-1);
+    vector<vector<int>>t (arr.size()+1,vector<int>(arr.size()+1, - 1));
+    return solve(arr,1,arr.size()-1,t);
+        
     }
 };
 
